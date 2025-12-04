dda8a9092060621002ac96e246571737a3079ec2
yashV131 <mehta.yashvi@gmail.com>
2025-12-03 18:03:05 -0600

    Added real-time mouse coordinate tracking and two different layouts for both players

--- Dashboard.py
+++ Dashboard.py
@@ -36,24 +36,46 @@ rect_x2 = rect_x1 + rect_width+20
 CELL_SIZE = 20
 
 #draw the grid for battleships
-def draw_grid(surface, x0, y0, grid, reveal=False):
-    for x in range(20):
-        for y in range(20):
-            rect = pygame.Rect(x0 + x * CELL_SIZE, y0 + y * CELL_SIZE, CELL_SIZE - 1, CELL_SIZE - 1)
-            color = (200, 200, 200)
-            pygame.draw.rect(surface, color, rect)
-
+def draw_grid(surface, x0, y0, grid,reveal=False):
+    if(reveal):
+        for x in range(20):
+            for y in range(20):
+                rect = pygame.Rect(x0 + x * CELL_SIZE, y0 + y * CELL_SIZE, CELL_SIZE-1, CELL_SIZE-1)
+                color = (200, 200, 200)
+                pygame.draw.rect(surface, color, rect)
+    else:
+        rect = pygame.Rect(x0, y0, CELL_SIZE*20-1, CELL_SIZE*20-1)
+        color = (0,0,0)
+        pygame.draw.rect(surface, color, rect)
+        text_surface = font.render('It is not your turn yet!', True, (255, 255, 255))
+        screen.blit(text_surface, (x0+100, y0+190)) 
+        
 # Game loop
 running = True
 while running:
     # Fill the background
     screen.fill((0,0,0))
-
-    # Draw the boxes for Player 1 and Player 2
-    text_surface = font.render('Player 1, place your ships!', True, (255, 255, 255))
-    screen.blit(text_surface, (rect_x1, rect_y1-40))   
-    draw_grid(screen, rect_x1, rect_y1, {}, reveal=True)
-    draw_grid(screen, rect_x2, rect_y1, {}, reveal=True)
+    
+    #Display mouse coordinates    
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    mouse_text = font.render(f' X,Y:({mouse_x}, {mouse_y})', True, WHITE)
+    screen.blit(mouse_text, (800,500))
+    
+    reveal1 = False 
+    if(reveal1):
+        # Player 1's turn
+        text_surface = font.render('Player 1, place your ships!', True, (255, 255, 255))
+        screen.blit(text_surface, (rect_x1, rect_y1-40))  
+        
+        draw_grid(screen, rect_x1, rect_y1, {},reveal1)
+        draw_grid(screen, rect_x2, rect_y1, {})
+    else:
+        # Player 2's turn
+        text_surface = font.render('Player 2, place your ships!', True, (255, 255, 255))
+        screen.blit(text_surface, (rect_x2, rect_y1-40))  
+        
+        draw_grid(screen, rect_x1, rect_y1, {})
+        draw_grid(screen, rect_x2, rect_y1, {},True)
     pygame.draw.rect(screen, BLUE, (rect_x1, rect_y1, rect_width, rect_height), line_thickness)
     pygame.draw.rect(screen, BLUE, (rect_x2, rect_y1, rect_width, rect_height), line_thickness)
     
